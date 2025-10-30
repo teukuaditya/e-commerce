beb0f9e1f90ead3c997f0be4291424ed42592843
aditya <aditya20oktober@gmail.com>
2025-10-30 18:10:05 +0700

    fix bug

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -2,32 +2,6 @@
 <header class="header-v4">
     <!-- Header desktop -->
     <div class="container-menu-desktop">
-        <!-- Topbar -->
-        <!-- <div class="top-bar">
-            <div class="content-topbar flex-sb-m h-full container">
-                <div class="left-top-bar">
-                    Free shipping for standard order over $100
-                </div>
-
-                <div class="right-top-bar flex-w h-full">
-                    <a href="#" class="flex-c-m trans-04 p-lr-25">
-                        Help & FAQs
-                    </a>
-
-                    <a href="#" class="flex-c-m trans-04 p-lr-25">
-                        My Account
-                    </a>
-
-                    <a href="#" class="flex-c-m trans-04 p-lr-25">
-                        EN
-                    </a>
-
-                    <a href="#" class="flex-c-m trans-04 p-lr-25">
-                        USD
-                    </a>
-                </div>
-            </div>
-        </div> -->
 
         <div class="wrap-menu-desktop how-shadow1">
             <nav class="limiter-menu-desktop container">
@@ -91,7 +65,7 @@
                     <a href="#"
                         class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti"
                         data-notify="0">
-                        <i class="zmdi zmdi-favorite-outline"></i>
+                    <i class="zmdi zmdi-favorite-outline"></i>
                     </a>
 
                     {{-- <!-- Garis Pemisah -->
